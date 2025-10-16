3da365a002fa18907cb10c6c42e3f6da934f5e93
Damian <josepwnz@gmail.com>
2025-10-17 00:15:02 +0200

    added more shadcn components

---
+++ src/components/spinner/FullScreenLoadingSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="fixed inset-0 z-50 bg-white/70 backdrop-blur-sm flex items-center justify-center"
+  >
+    <div
+      class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"
+    ></div>
+  </div>
+</template>
---
+++ src/components/spinner/LoadingSpinner.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center items-center h-full w-full">
+    <div
+      class="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"
+    ></div>
+  </div>
+</template>
