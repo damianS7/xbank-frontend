04cc2d3d1ffcaa06e6a5b82aeba8a1d2464a63b8
Damian <josepwnz@gmail.com>
2025-10-18 02:24:29 +0200

    replaced composition store for setup

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,121 +1,64 @@
-import type { Customer } from "@/types/Customer";
+import type { JwtPayload } from "@/types/JwtPayload";
+import type { UserRegisterRequest } from "@/types/request/UserRegisterRequest";
+import { jwtDecode } from "jwt-decode";
 import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+import { authService } from "@/services/authService";
+import router from "@/router";
 
-export const useAuthStore = defineStore("auth", {
-  state: () => ({
-    token: "",
-    initialized: false,
-  }),
+export const useAuthStore = defineStore("auth", () => {
+  const token = ref("");
+  const initialized = ref(false);
 
-  getters: {
-    isAuthenticated: (state) => {
-      if (state.token === "") {
-        return false;
-      }
+  const isAuthenticated = computed(
+    () => token.value !== "" && initialized.value === true
+  );
 
-      return true;
-    },
-  },
+  async function initialize() {
+    const storedToken = localStorage.getItem("token") ?? "";
+    try {
+      await authService.validateToken(storedToken);
+      token.value = storedToken;
+      initialized.value = true;
+    } catch (error) {
+      // logout();
+    }
+  }
 
-  actions: {
-    async login(email: string, password: string) {
-      try {
-        const response = await fetch(
-          `${process.env.VUE_APP_API_URL}/auth/login`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email, password }),
-          }
-        );
+  async function login(email: string, password: string) {
+    try {
+      const jwtToken = await authService.login(email, password);
+      token.value = jwtToken;
+      localStorage.setItem("token", jwtToken);
+    } catch (error) {
+      throw error;
+    }
+  }
 
-        // if response is not 200, throw an error
-        if (response.status !== 200) {
-          const error = await response.json();
-          throw new Error(error.message || "Invalid credentials.");
-        }
+  async function register(fields: UserRegisterRequest) {
+    return await authService.register(fields);
+  }
 
-        const data = await response.json();
-        this.token = data.token;
-        localStorage.setItem("token", this.token);
-      } catch (error) {
-        if (error instanceof Error) {
-          throw error;
-        }
-        throw new Error("Invalid credentials.");
-      }
-    },
-    async register(fields: Customer) {
-      try {
-        const response = await fetch(
-          `${process.env.VUE_APP_API_URL}/auth/register`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(fields),
-          }
-        );
+  async function logout() {
+    token.value = "";
+    initialized.value = false;
+    localStorage.setItem("token", "");
+    router.push({ name: "login" });
+  }
 
-        // if response is not 201, throw an error
-        if (response.status !== 201) {
-          const error = await response.json();
-          throw new Error(error.message || "Registration failed.");
-        }
+  function getPayload() {
+    const token = localStorage.getItem("token");
+    return token ? jwtDecode<JwtPayload>(token) : null;
+  }
 
-        const data = await response.json();
-        return { status: response.status, data };
-      } catch (error) {
-        if (error instanceof Error) {
-          throw error;
-        }
-        throw new Error("Registration failed.");
-      }
-    },
-    async isTokenValid(token: string) {
-      try {
-        const response = await fetch(
-          `${process.env.VUE_APP_API_URL}/auth/token/validate`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        // if response is not 200, throw an error
-        if (response.status !== 200) {
-          const error = await response.json();
-          throw new Error(error.message || "Token validation failed.");
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          throw error;
-        }
-        throw new Error("Token validation failed.");
-      }
-    },
-    async logout() {
-      this.token = "";
-      this.initialized = false;
-      localStorage.clear();
-    },
-    async initialize() {
-      const savedToken = localStorage.getItem("token");
-      if (savedToken) {
-        this.token = savedToken;
-
-        await this.isTokenValid(savedToken).catch(() => {
-          this.logout();
-          return;
-        });
-      }
-      this.initialized = true;
-    },
-  },
+  return {
+    token,
+    initialized,
+    isAuthenticated,
+    login,
+    register,
+    logout,
+    initialize,
+    getPayload,
+  };
 });
