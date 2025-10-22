d26c5533208c2e08fc9985fe9b2aca322b8decf0
Damian <josepwnz@gmail.com>
2025-10-23 01:00:38 +0200

    added custom avatar component

---
+++ src/components/CustomAvatar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+defineProps<{
+  src?: string;
+  fallback?: string;
+}>();
+</script>
+<template>
+  <Avatar>
+    <AvatarImage v-if="src" :src="src" />
+    <AvatarFallback v-if="fallback" class="bg-blue-500">{{
+      fallback.charAt(0)
+    }}</AvatarFallback>
+  </Avatar>
+</template>
