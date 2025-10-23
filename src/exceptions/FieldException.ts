e43be51a09af5dd040d38473a5f94e48eaaf0f8e
Damian <josepwnz@gmail.com>
2025-10-23 17:10:13 +0200

    removed unused

--- src/exceptions/FieldException.ts
+++
@@ -1,9 +0,0 @@
-export class FieldException extends Error {
-  public errors: Record<string, string[]>;
-
-  constructor(message: string, errors: Record<string, string[]>) {
-    super(message);
-    this.name = "FieldException";
-    this.errors = errors;
-  }
-}
