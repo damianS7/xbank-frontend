30743994a1d920451143952ac536902e6263c11c
Damian <josepwnz@gmail.com>
2025-11-04 22:14:46 +0100

    added settings tab

---
+++ src/views/settings/components/SettingsAccountTab.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { useSettingStore } from "@/stores/setting";
+import Switch from "@/components/ui/switch/Switch.vue";
+import { TabsContent } from "@/components/ui/tabs";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+  CardDescription,
+} from "@/components/ui/card";
+
+// store
+const settingStore = useSettingStore();
+</script>
+<template>
+  <TabsContent value="account" class="">
+    <Card>
+      <CardHeader>
+        <CardTitle>Account</CardTitle>
+        <CardDescription> CardDescription </CardDescription>
+      </CardHeader>
+      <CardContent class="space-y-2">
+        <form class="space-y-6">
+          <!-- Enable 2FA -->
+          <div class="flex items-center justify-between">
+            <label class="text-lg font-medium"
+              >Enable Two-Factor Authentication (2FA)</label
+            >
+            <Switch
+              v-model="settingStore.settings.TWO_FACTOR_AUTHENTICATION"
+              class="data-[state=checked]:bg-blue-500"
+            />
+          </div>
+
+          <!-- Email Notifications -->
+          <div class="flex items-center justify-between">
+            <label class="text-lg font-medium">Email Notifications</label>
+            <Switch
+              v-model="settingStore.settings.EMAIL_NOTIFICATIONS"
+              class="data-[state=checked]:bg-blue-500"
+            />
+          </div>
+
+          <!-- Language Selector -->
+          <div
+            class="flex flex-col sm:flex-row sm:items-center justify-between"
+          >
+            <label class="text-lg font-medium mb-2 sm:mb-0">Language</label>
+            <select
+              class="border rounded p-2 w-full sm:w-auto"
+              v-model="settingStore.settings.LANGUAGE"
+            >
+              <option value="ES">Español</option>
+              <option value="EN">English</option>
+            </select>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter> </CardFooter>
+    </Card>
+  </TabsContent>
+</template>
---
+++ src/views/settings/components/SettingsSecurityTab.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useSettingStore } from "@/stores/setting";
+import { TabsContent } from "@/components/ui/tabs";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+  CardDescription,
+} from "@/components/ui/card";
+// store
+const settingStore = useSettingStore();
+</script>
+<template>
+  <TabsContent value="security" class="">
+    <Card>
+      <CardHeader>
+        <CardTitle>Account</CardTitle>
+        <CardDescription> CardDescription </CardDescription>
+      </CardHeader>
+      <CardContent class="space-y-2"> Security </CardContent>
+      <CardFooter> </CardFooter>
+    </Card>
+  </TabsContent>
+</template>
