7c83457c74dd5d2b2a13be21f59b85c0cd85ae48
Damian <josepwnz@gmail.com>
2025-11-05 21:12:26 +0100

    centered on y and x axis

--- src/views/banking/card/BankingCardListView.vue
+++ src/views/banking/card/BankingCardListView.vue
@@ -4,6 +4,7 @@ import { useCardStore } from "@/stores/card";
 import BankingCardFront from "@/views/banking/card/components/BankingCardFront.vue";
 import { ChevronRight, ChevronLeft } from "lucide-vue-next";
 import PageLayout from "@/layouts/PageLayout.vue";
+import CustomAlert from "@/components/CustomAlert.vue";
 
 // ---
 
@@ -37,33 +38,35 @@ function nextCard() {
     <template #content>
       <CustomAlert ref="alert" />
 
-      <slot v-if="cards && currentCard">
-        <div class="flex flex-col items-center">
-          <router-link
-            :to="{ name: 'banking-card', params: { id: currentCard.id } }"
+      <div class="flex flex-col my-auto mx-auto gap-2">
+        <slot v-if="cards && currentCard">
+          <div class="flex flex-col items-center">
+            <router-link
+              :to="{ name: 'banking-card', params: { id: currentCard.id } }"
+            >
+              <BankingCardFront class="shadow-md w-full" :card="currentCard" />
+            </router-link>
+          </div>
+
+          <div
+            class="flex mx-auto items-center p-1 text-white bg-blue-500 rounded-lg"
           >
-            <BankingCardFront class="shadow-md w-full" :card="currentCard" />
-          </router-link>
+            <button class="btn-sm cursor-pointer" @click="previousCard">
+              <ChevronLeft />
+            </button>
+            <span> {{ currentCardIndex + 1 }} / {{ cards.length }} </span>
+            <button class="btn-sm cursor-pointer" @click="nextCard">
+              <ChevronRight />
+            </button>
+          </div>
+        </slot>
+
+        <div v-else-if="!currentCard" class="text-gray-600 text-center">
+          Loading card ...
         </div>
 
-        <div
-          class="flex mx-auto items-center p-1 text-white bg-blue-500 rounded-lg"
-        >
-          <button class="btn-sm cursor-pointer" @click="previousCard">
-            <ChevronLeft />
-          </button>
-          <span> {{ currentCardIndex + 1 }} / {{ cards.length }} </span>
-          <button class="btn-sm cursor-pointer" @click="nextCard">
-            <ChevronRight />
-          </button>
-        </div>
-      </slot>
-
-      <div v-else-if="!currentCard" class="text-gray-600 text-center">
-        Loading card ...
+        <div v-else class="text-gray-600 text-center">No card to show ...</div>
       </div>
-
-      <div v-else class="text-gray-600 text-center">No card to show ...</div>
     </template>
   </PageLayout>
 </template>
