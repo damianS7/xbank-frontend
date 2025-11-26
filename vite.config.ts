c93d8401a58c03f273eeffb309b4e59866f7eb22
Damian <josepwnz@gmail.com>
2025-11-26 23:57:24 +0100

    added allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true,
-    port: 8080,
-    allowedHosts: ["localhost", "whatsapp.local"],
+    port: 8081,
+    allowedHosts: ["localhost", "xbank.local"],
   },
   plugins: [vue(), tailwindcss()],
   define: {
-    global: 'window'
+    global: "window",
   },
   resolve: {
     alias: {
