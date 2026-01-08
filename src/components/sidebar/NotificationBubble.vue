5f985037a883e7c344bfd88edc600511169f1e27
Damian <josepwnz@gmail.com>
2026-01-08 03:58:21 +0100

    show bubble only with >0 notifications

--- src/components/sidebar/NotificationBubble.vue
+++ src/components/sidebar/NotificationBubble.vue
@@ -11,6 +11,7 @@ const { countNotifications: unreadNotifications } =
 </script>
 <template>
   <span
+    v-if="unreadNotifications > 0"
     class="absolute -top-1 -right-1 flex items-center justify-center min-w-4 h-4 px-1 rounded-full bg-red-600 text-white text-[12px] leading-none font-semibold shadow-sm pointer-events-none"
     :class="[showNotifications ? 'bg-gray-300 rounded-full' : '']"
   >
