4cc556686a3cb509f8865f07f951a03cfa0dcefb
Damian <josepwnz@gmail.com>
2026-01-08 03:33:34 +0100

    added scroll

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -3,6 +3,7 @@ import { useAccountStore } from "@/stores/account";
 import { computed } from "vue";
 import { BankingAccountCurrencies as currencies } from "@/types/BankingAccount";
 import BankingAccountCurrencyChart from "@/views/home/components/BankingAccountCurrencyChart.vue";
+import PageLayout from "@/layouts/PageLayout.vue";
 
 const accountStore = useAccountStore();
 const accounts = computed(() => accountStore.bankingAccounts);
@@ -16,40 +17,49 @@ function totalAccountBalanceByCurrency(currency: string) {
 }
 </script>
 <template>
-  <div v-if="accounts" class="flex flex-col gap-4">
-    <div class="grid grid-cols-1 md:grid-cols-2 dash-item-group">
-      <h3 class="font-semibold md:col-span-2">Summary</h3>
+  <div class="grid grid-rows-[auto_1fr] h-full border shadow rounded-md">
+    <section
+      id="page-section-content"
+      class="pg-section-content flex flex-col gap-4 overflow-auto h-full"
+    >
+      <div v-if="accounts" class="flex flex-col gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 dash-item-group">
+          <h3 class="font-semibold md:col-span-2">Summary</h3>
 
-      <!-- Accounts and Cards -->
-      <div class="flex justify-between dash-item">
-        <span>Accounts</span>
-        <span>{{ accountStore.countAccounts }}</span>
-      </div>
-      <div class="flex justify-between dash-item">
-        <span>Cards</span>
-        <span>{{ accountStore.countCards }}</span>
-      </div>
-    </div>
+          <!-- Accounts and Cards -->
+          <div class="flex justify-between dash-item">
+            <span>Accounts</span>
+            <span>{{ accountStore.countAccounts }}</span>
+          </div>
+          <div class="flex justify-between dash-item">
+            <span>Cards</span>
+            <span>{{ accountStore.countCards }}</span>
+          </div>
+        </div>
 
-    <!-- Balance -->
-    <div class="grid grid-cols-1 md:grid-cols-2 dash-item-group">
-      <h3 class="font-semibold md:col-span-2">Account balances by currency</h3>
-      <div
-        v-for="(currency, index) in currencies"
-        :key="index"
-        class="flex flex-col my-1 p-1 rounded"
-      >
-        <div>
-          <BankingAccountCurrencyChart :currency="currency" />
-        </div>
-        <div class="flex justify-between mt-4 text-sm dash-item">
-          <span>Total</span>
-          <span class="text-green-600 justify-end">
-            {{ totalAccountBalanceByCurrency(currency) }} {{ currency }}
-          </span>
+        <!-- Balance -->
+        <div class="grid grid-cols-1 lg:grid-cols-2 dash-item-group">
+          <h3 class="font-semibold lg:col-span-2">
+            Account balances by currency
+          </h3>
+          <div
+            v-for="(currency, index) in currencies"
+            :key="index"
+            class="flex flex-col my-1 p-1 rounded"
+          >
+            <div>
+              <BankingAccountCurrencyChart :currency="currency" />
+            </div>
+            <div class="flex justify-between mt-4 text-sm dash-item">
+              <span>Total</span>
+              <span class="text-green-600 justify-end">
+                {{ totalAccountBalanceByCurrency(currency) }} {{ currency }}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 <style scoped>
